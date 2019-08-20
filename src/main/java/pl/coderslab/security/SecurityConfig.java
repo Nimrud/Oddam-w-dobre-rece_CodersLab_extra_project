5fa2863f5a3e1e123ff2649da49da534ac652dfa
Krzysztof Jaczewski <krzyysiek@poczta.onet.pl>
2019-08-20 23:43:16 +0200

    Spring Security - dostęp do adresów dla poszczególnych ról

--- src/main/java/pl/coderslab/security/SecurityConfig.java
+++ src/main/java/pl/coderslab/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.coderslab.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,10 +10,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("newUser").password("{noop}user222").roles("USER")
                 .and()
                 .withUser("mainAdmin").password("{noop}admin333").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/institutions/**", "/categories/**").hasRole("ADMIN")
+                .antMatchers("/donations/**", "/categories/**", "/institutions/**").authenticated()
+                .and().formLogin();
     }
 }
