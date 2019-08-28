2a83c4497243c0f23d0d1e5a62dfac0698f26ae3
Krzysztof Jaczewski <krzyysiek@poczta.onet.pl>
2019-08-28 18:42:00 +0200

    Spring Security - zarządzanie użytkownikami w oparciu o bazę danych (1)

--- src/main/java/pl/coderslab/security/SecurityConfig.java
+++ src/main/java/pl/coderslab/security/SecurityConfig.java
@@ -1,10 +1,13 @@
 package pl.coderslab.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import pl.coderslab.users.SpringDataUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -29,5 +32,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().logout().logoutSuccessUrl("/")
                 .permitAll()
                 .and().exceptionHandling().accessDeniedPage("/403");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SpringDataUserDetailsService customUserDetailsService(){
+        return new SpringDataUserDetailsService();
     }
 }
---
+++ src/main/java/pl/coderslab/users/UserRepo.java
@@ -0,0 +1,9 @@
+package pl.coderslab.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
